cc10e4b9a6fce1a8322810ddda82344616dd3634
Tomas R J <49104467+TomasRJ@users.noreply.github.com>
2023-06-13 10:53:23 +0200

    Overgang fra dev to prod

--- frontend.Dockerfile
+++ frontend.Dockerfile
@@ -5,4 +5,7 @@ COPY frontend ./frontend
 WORKDIR /srv/app/frontend
 RUN npm i
 EXPOSE 8000
-CMD [ "npm", "run", "dev" ]
+RUN npm run build
+RUN npm install -g serve
+WORKDIR /srv/app/frontend/dist
+CMD [ "serve", "-s", "-l", "8000" ]
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,12 +5,12 @@ import App from './App.vue';
 import router from './router';
 
 const config = {
-  development: {
+  production: {
     api: 'http://localhost:3000',
   },
 };
 
 createApp(App)
-  .provide('api', config[import.meta.env.MODE || 'development'].api)
+  .provide('api', config[import.meta.env.MODE || 'production'].api)
   .use(router)
   .mount('#app');
